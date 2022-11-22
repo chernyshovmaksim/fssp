f4fefde306742e18dc5b3a6caf5fcbb863c20865
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2022-11-23 00:02:10 +0500

    add tailwind to project

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <span className="text-red-500">tailwind</span>
+    </>
   )
 }
