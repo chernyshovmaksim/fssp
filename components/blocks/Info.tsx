6f88bc8d4aed024775ddd1ac0a9274ca0053899c
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2022-11-24 18:07:21 +0500

    added component - info

---
+++ components/blocks/Info.tsx
@@ -0,0 +1,70 @@
+import { FC } from "react";
+import { HiBellAlert } from "react-icons/hi2";
+import { HiSearch } from "react-icons/hi";
+import { FaRegCreditCard } from "react-icons/fa";
+import { SiAdguard } from "react-icons/si";
+
+const Info: FC = () => {
+  return (
+    <>
+      <section className="info py-8 bg-gray-200">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-4 gap-y-12">
+          <div className="flex items-center gap-2">
+            <div>
+              <HiBellAlert className="text-emerald-700 text-6xl" />
+            </div>
+            <div>
+              <h4 className="text-2xl font-bold text-gray-700">
+                Уведомления о штрафах и погашениях
+              </h4>
+              <span className="text-gray-500">
+                Мы поможем удостовериться в погашении и сообщим о статусе
+              </span>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <div>
+              <FaRegCreditCard className="text-emerald-700 text-6xl" />
+            </div>
+            <div>
+              <h4 className="text-2xl font-bold text-gray-700">
+                Быстрая оплата онлайн
+              </h4>
+              <span className="text-gray-500">
+                Оплата на счет казначейства картой любого банка в тот же день
+              </span>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <div>
+              <HiSearch className="text-emerald-700 text-6xl" />
+            </div>
+            <div>
+              <h4 className="text-2xl font-bold text-gray-700">
+                Система мониторинга новых штрафов
+              </h4>
+              <span className="text-gray-500">
+                Система ищет информацию о новых штрафах
+              </span>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <div>
+              <SiAdguard className="text-emerald-700 text-6xl" />
+            </div>
+            <div>
+              <h4 className="text-2xl font-bold text-gray-700">
+                Безопасные платежи
+              </h4>
+              <span className="text-gray-500">
+                Списание и зачисление средств
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Info;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 // import styles from '../styles/Home.module.scss'
 
 import MainLayout from "../components/layouts/MainLayout";
-import Fancybox from "./../components/ui/Fancybox";
 import Action from "../components/blocks/Action";
+import Info from "../components/blocks/Info";
 
 export default function Home() {
     const title = "Узнать задолженность у судебных приставов: по фамилии, алиментам или номеру постановления"
-    const link = `https://reg.checkyour.name/fines_96/?phone=79225391269&click_id=1669269877461`
+    // const link = `https://reg.checkyour.name/fines_96/?phone=79225391269&click_id=1669269877461`
     return (
         <>
             <MainLayout title={title}>
                 <Action />
+                <Info />
             </MainLayout>
         </>
     )
