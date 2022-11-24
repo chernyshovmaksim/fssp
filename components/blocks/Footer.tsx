08ade8917957e54ca3f434216ba851187a3c2b5a
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2022-11-24 20:21:55 +0500

    added component footer

--- components/blocks/Footer.tsx
+++ components/blocks/Footer.tsx
@@ -1,11 +1,24 @@
 import {FC} from "react";
+import Link from "next/link";
 
 const Footer:FC = () => {
     return(
         <>
             <footer className="bg-gray-200 py-16">
                 <div className="container mx-auto">
-                    Footer
+                    <div>
+                        <h5 className="text-xl font-bold text-center">Долги по ФССП</h5>
+                        <span className="text-gray-500 text-xs text-center block">Поиск долгов в банке данных судебных приставов</span>
+                    </div>
+                    <div className="mt-4 flex flex-col items-center text-xs">
+                        <Link href={'/'}>Обработка персональных данных</Link>
+                        <Link href={'/'}>Политика конфиденциальности</Link>
+                    </div>
+                    <div className="flex flex-col items-center text-xs text-gray-500 mt-8">
+                        <div>© 2022 Все права защищены.</div>
+                        <div>Контактный телефон: +7 (910) 444-77-66</div>
+                        <div>E-mail: moi-dolgi1@gmail.com</div>
+                    </div>
                 </div>
             </footer>
         </>
