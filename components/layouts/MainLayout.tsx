c3d6c942933c4a888f6b6e4ace8271739feed966
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2022-11-24 11:21:24 +0500

    add Fancybox and add iframe for testing in index page

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -2,26 +2,36 @@ import {FC, ReactNode} from "react";
 import Head from "next/head";
 import Header from "../blocks/Header";
 import Navigation from "../blocks/Navigation";
+import NavigationStore from "../../store/NavigationStore";
 
-interface IMainLayout{
+interface IMainLayout {
     title: string
     children: ReactNode
 }
 
-const MainLayout:FC<IMainLayout> = ({children, title}) => {
+const MainLayout: FC<IMainLayout> = ({children, title}) => {
+
+    const NavigationStatus = NavigationStore(state => state.status)
+    let classListBody = ['min-h-screen']
+
+    if(NavigationStatus){
+        classListBody.push('overflow-hidden')
+    }
 
     return (
         <>
-            <Head>
-                <title>
-                    {title}
-                </title>
-            </Head>
+            <div className={classListBody.join(' ')}>
+                <Head>
+                    <title>
+                        {title}
+                    </title>
+                </Head>
 
-            <Header />
-            <Navigation />
+                <Header/>
+                <Navigation/>
 
-            <div className={'container mx-auto'}>{children}</div>
+                <div className={'container mx-auto'}>{children}</div>
+            </div>
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
-
 // import styles from '../styles/Home.module.scss'
 
 import MainLayout from "../components/layouts/MainLayout";
+import Fancybox from "./../components/ui/Fancybox";
 
 export default function Home() {
-  const title = "Узнать задолженность у судебных приставов: по фамилии, алиментам или номеру постановления"
-  return (
-    <>
-    <MainLayout title={title}>
-      layout
-    </MainLayout>
-    </>
-  )
+    const title = "Узнать задолженность у судебных приставов: по фамилии, алиментам или номеру постановления"
+    return (
+        <>
+            <MainLayout title={title}>
+                <Fancybox>
+                    <a className={"btn"}
+                        href="https://reg.checkyour.name/fines_96/?phone=79225391269&click_id=1669269877461"
+                        data-fancybox
+                        data-type="iframe"
+                        data-preload="false"
+                        data-width="90%"
+                        data-height="90%"
+                    >
+                        Запрос
+                    </a>
+                </Fancybox>
+            </MainLayout>
+        </>
+    )
 }
