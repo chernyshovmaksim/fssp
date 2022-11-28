84c35a85d14a8fe6c8c82fc9702a4ea01498cbb9
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2022-11-28 21:30:33 +0500

    updated Bases component

--- components/blocks/Bases.tsx
+++ components/blocks/Bases.tsx
@@ -12,59 +12,36 @@ const Bases: FC = () => {
       <section className="bases py-32">
         <div className="container mx-auto">
           <h3 className="text-3xl font-bold text-gray-900 mb-8">
-            Доступные базы для поиска и мониторинга
+            Сервисы для граждан РФ
           </h3>
-          <div className="grid grid-cols-1 gap-y-16 md:grid-cols-3 md:gap-4">
-            <Link href={"https://xn--90adear.xn--p1ai/"} className="flex items-center gap-4 border rounded p-4 shadow">
-              <Image
-                src={gibddLogo.src}
-                width={60}
-                height={60}
-                alt={
-                  "Государственная инспекция безопасности дорожного движения"
-                }
-              />
-              <div className="flex flex-col flex-1">
-                <h4 className="text-xl font-bold text-gray-700">
-                  Государственная инспекция безопасности дорожного движения
-                </h4>
-                <span className=" text-xs text-gray-500">
-                  Поиск штрафов по стс и гос номерам
-                </span>
-              </div>
-            </Link>
-            <div className="flex items-center gap-4 border rounded p-4 shadow">
-              <Image
-                src={fnsLogo.src}
-                width={60}
-                height={60}
-                alt={"Федеральная налоговая служба"}
-              />
-
-              <div className="flex flex-col flex-1">
-                <h4 className="text-xl font-bold text-gray-700">
-                  Федеральная налоговая служба
-                </h4>
-                <span className="text-xs text-gray-500">
-                  Предоставление сведений из ЕГРЮЛ/ЕГРИП
-                </span>
-              </div>
+          <div className="grid grid-cols-1 gap-y-16 md:grid-cols-2 md:gap-16">
+            <div>
+              <h3 className="text-xl text-gray-400 pb-4">Проверка задолженностей по базе ФССП</h3>
+              <p className="text-xs text-gray-600">
+                Бесплатная онлайн проверка задолженностей по судебным приставам
+                с возможностью погасить долг онлайн прямо на сайте
+              </p>
             </div>
-            <div className="flex items-center gap-4 border rounded p-4 shadow">
-                <Image
-                  src={fsspLogo.src}
-                  width={60}
-                  height={60}
-                  alt={"Федеральная служба судебных приставов"}
-                />
-              <div className="flex flex-col flex-1">
-                <h4 className="text-xl font-bold text-gray-700">
-                  Федеральная служба судебных приставов
-                </h4>
-                <span className="text-xs text-gray-500">
-                  Поиск по ФИО и номерам исполнительных производств
-                </span>
-              </div>
+            <div>
+              <h3 className="text-xl text-gray-400 pb-4">Проверка запрета на выезд за границу</h3>
+              <p className="text-xs text-gray-600">
+                Инструкция, как проверить наличие запрета на выезд за границу и
+                в каких случаях пристав может наложить арест или запрет
+              </p>
+            </div>
+            <div>
+              <h3 className="text-xl text-gray-400 pb-4">Реестр лиц, находящихся в розыске ФССП</h3>
+              <p className="text-xs text-gray-600">
+                База физических лиц, находящихся в федеральном розыске Службой
+                Судебных Приставов
+              </p>
+            </div>
+            <div>
+              <h3 className="text-xl text-gray-400 pb-4">Проверка налоговой задолженности по ИНН</h3>
+              <p className="text-xs text-gray-600">
+                Бесплатная онлайн проверка задолженностей по судебным приставам
+                с возможностью погасить долг онлайн прямо на сайте
+              </p>
             </div>
           </div>
         </div>
